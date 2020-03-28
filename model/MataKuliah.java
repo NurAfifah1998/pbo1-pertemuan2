a6ef15185bbf63e1a97029e8119f95f5d4570df7
NurAfifah1998 <NurAfifah1998>
2020-03-28 09:10:22 +0800

    Memanggil method dari banyak class di package lain

---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println(" ========Data MataKuliah========="); 
+  System.out.println("Kode MataKuliah	: TIF");
+  System.out.println("Nama MataKuliah	: Pemograman Berbasis Objek 1");
+  System.out.println("===============================");  
+ }
+}
